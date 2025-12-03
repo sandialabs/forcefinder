9adfaad60bb880ef87590c672f268eeb4d34e813
Steven Carter <spcarte@sandia.gov>
2025-12-03 16:33:33 -0700

    Updated set-up file from legacy repo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="An advanced inverse source estimation package for Python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://cee-gitlab.sandia.gov/spcarte/forcefinder",
+    url="https://github.com/sandialabs/forcefinder",
     packages=find_packages(where='src'),  # Tell setuptools to look in the src/ folder
     package_dir={"": "src"},  # Set the base folder for the packages to src/
     classifiers=["Programming Language :: Python :: 3.11",
@@ -18,15 +18,24 @@ setup(
                  "Operating System :: Microsoft :: Windows :: Windows 11",
                  "Operating System :: MacOS :: MacOS X"
                  ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=[
         "numpy>=1.26.0",
         "scipy>=1.10.0",
         "sdynpy>=0.14.0",
         "joblib>=1.3.0",
         "matplotlib>=3.5.0",
+        "cvxpy>=1.7",
         "pyqt5"
         ],
+    extras_require={ # Extra packages that are useful for development
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "jupyter-book",
+            "sphinx-autoapi"
+        ]
+    }
 )
 
 
